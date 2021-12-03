cb72c514d015d5fe5754033c54ab8e83d2fb53b4
Chaitanya Kumar <chaitanyak28771@gmail.com>
2021-12-03 15:05:36 +0100

    Fix M/M/n analytical functions, add M/H/1 analytical functions

--- assignment_analytical.py
+++ assignment_analytical.py
@@ -25,11 +25,11 @@ def MMn_utilization(λ, μ, n):
 def MMn_p0(λ, μ, n):
     ρ = MMn_utilization(λ, μ, n)
     assert 0 < ρ < 1, "ρ not in (0,1)"
-    n_factorial = factorial(n)
-    series_term = lambda k: np.power(n*ρ, k) / n_factorial
-    series_sum = np.sum(series_term(range(n)))
+    series_term = lambda k: np.power(n*ρ, k) / factorial(k)
+    sequence = np.array([series_term(k) for k in range(n)])
+    series_sum = np.sum(sequence)
     
-    p0_inv = series_sum + series_term(n) * ρ/(1-ρ)
+    p0_inv = series_sum + series_term(n)/(1-ρ)
     p0 = 1/p0_inv
     return p0
     
@@ -43,13 +43,16 @@ def MMn_pk(λ, μ, n, k):
     return pk
     
     
-def MMn_mean_customer_count(λ, μ, n):
+def MMn_mean_queue_length(λ, μ, n):
     """
     Asymptotic mean customer count for M/M/n queue
     """
-    raise NotImplementedError
     ρ = MMn_utilization(λ, μ, n)
-    assert 0 < ρ < 1, "ρ not in (0,1)"
+    p0 = MMn_p0(λ, μ, n)
+    nρ = n * ρ
+    L_Q = np.power(nρ, n+1) * p0/(n * factorial(n) * np.power(1 - ρ, 2))
+    return L_Q
+    
     
 def MMn_queueing_prob(λ, μ, n):
     ρ = MMn_utilization(λ, μ, n)
@@ -57,15 +60,28 @@ def MMn_queueing_prob(λ, μ, n):
     P_Q = pn / (1-ρ)
     return P_Q
 
-def MMn_mean_queue_length(λ, μ, n):
-    ρ = MMn_utilization(λ, μ, n)
-    P_Q = MMn_queueing_prob(λ, μ, n)
-    N_q = P_Q * ρ / (1 - ρ)
-    return N_q
     
 def MMn_mean_waiting_time(λ, μ, n):
-    ρ = MMn_utilization(λ, μ, n)
-    P_Q = MMn_queueing_prob(λ, μ, n)
-    N_q = MMn_mean_queue_length(λ, μ, n)
-    T_q = N_q / λ
-    return T_q
+    L_Q = MMn_mean_queue_length(λ, μ, n)
+    T_Q = L_Q / λ
+    return T_Q
+
+def MH1_soujorn_time(λ, μs, ps):
+    μ_inv = np.sum(ps/μs)
+    μ = 1/μ_inv
+    ρ = λ*μ_inv
+    var = 2*np.sum(ps/np.power(μs, 2)) - μ_inv**2 
+    L = ρ + (ρ**2 * (1 + var * μ**2))/(2*(1 - ρ))
+    return L/λ
+
+def MH1_waiting_time(λ, μs, ps):
+    μ_inv = np.sum(ps/μs)
+    return MM1_sojourn_time(λ, μs, ps) - μ_inv
+
+def MD1_soujorn_time(λ, μ):
+    ρ = λ/μ
+    L = ρ + ρ**2/(2*(1 - ρ))
+    return L/λ
+
+def MD1_waiting_time(λ, μ):
+    return MD1_soujorn_time(λ, μ) - 1/μ
